52491cb839b9c89c621bff7b1c5f166a0271e81e
Yttrium69 <47668106+Yttrium69@users.noreply.github.com>
2023-02-20 06:13:29 +0900

    signup function

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app=express();
 
 const path=require('path');
 
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+
 var http=require('http').Server(app);
 const port=3000;
 
@@ -18,6 +21,14 @@ app.get("/yeojun", (req, res)=>{
 app.get("/gandan", (req, res)=>{
     res.sendFile(path.join(__dirname, "/gandan.html"))
 });
+
+app.get("/signup", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/signup.html"))
+});
+
+app.post("/code", (req, res)=>{
+    res.send(req.body.code);
+})
 
 http.listen(port, ()=>{
     console.log(`server listening on*:${port}`)
